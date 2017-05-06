6a728b699f3779d96419b7731a0dc624a2fd8daf
Uli <root@localhost>
2017-05-06 12:31:32 +0800

    changed

--- python/bottle/hello_world.py
+++ python/bottle/hello_world.py
@@ -1,10 +1,13 @@
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route
+from bottle import default_app, route, run
 
 @route('/')
 def hello_world():
     return 'Hello from Bottle!'
 
 application = default_app()
+
+run(server='wsgiref', host="0.0.0.0", port=8080)
+
 
 
