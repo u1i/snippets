046ed272ddbecafc2ad3181307b1c5e1e783484f
Uli <uli.hitzel@gmail.com>
2018-11-19 23:44:19 +0800

    Update file_read.py

--- python/basics/file_read.py
+++ python/basics/file_read.py
@@ -1,3 +1,10 @@
 with open("file.txt", mode='rb') as file_handle:
 	file_content = file_handle.read()
 file_handle.close()
+
+# or 
+import fileinput
+
+for line in fileinput.input():
+    print line
+
